8b2a003c6e07d12e1fa9527cf35406adf0a4df81
gerardbv <gerardballet@outlook.com>
2022-05-18 23:23:21 +0200

    added foreign keys in table definition

--- src/sqlTools/helpers/sql/getTableDefinition.sql
+++ src/sqlTools/helpers/sql/getTableDefinition.sql
@@ -102,4 +102,68 @@ FOR XML PATH('')
 
 
 
+set @query=@query+(
+select replace(replace(STUFF((
+select 'ALTER TABLE ['+main_table_schema+'].['+main_table+']  WITH CHECK ADD  CONSTRAINT ['+a.constraint_name+'] FOREIGN KEY('+a.main_table_cols+')'+char(13)+char(10)+
+
+			'REFERENCES ['+a.ref_table_schema+'].['+a.ref_table+'] ('+a.ref_table_cols+')'+char(13)+char(10)+
+			case when a.update_referential_action >0 then 'ON UPDATE '+a.update_referential_action_desc+char(13)+char(10) else '' end+
+			case when a.delete_referential_action >0 then 'ON DELETE '+a.delete_referential_action_desc+char(13)+char(10) else '' end+
+			'GO'+char(13)+char(10) from ( 
+
+	select 
+		fk.name COLLATE Latin1_General_CI_AS_KS_WS  as constraint_name
+		,main_schema.name COLLATE Latin1_General_CI_AS_KS_WS as main_table_schema
+		,main_table.name  COLLATE Latin1_General_CI_AS_KS_WS as main_table 	
+		,ref_schema.name COLLATE Latin1_General_CI_AS_KS_WS  as ref_table_schema
+		,ref_table.name  COLLATE Latin1_General_CI_AS_KS_WS as ref_table		
+		,fk.delete_referential_action_desc COLLATE Latin1_General_CI_AS_KS_WS as delete_referential_action_desc
+		,fk.update_referential_action_desc COLLATE Latin1_General_CI_AS_KS_WS  as update_referential_action_desc
+		,fk.delete_referential_action 
+		,fk.update_referential_action 
+		,   STUFF((
+			SELECT ', ' +'[' +table_col_main.name+']' 
+			
+				from  sys.foreign_key_columns fkc_main				
+				inner join sys.columns as table_col_main
+				on fkc_main.parent_object_id=table_col_main.object_id
+				and fkc_main.parent_column_id=table_col_main.column_id				
+				where fk.object_id=fkc_main.constraint_object_id
+				order by fkc_main.constraint_column_id
+
+			FOR XML PATH(''),TYPE).value('(./text())[1]','VARCHAR(MAX)')
+		  ,1,2,'') COLLATE Latin1_General_CI_AS_KS_WS AS main_table_cols
+		 ,   STUFF((
+			SELECT ', ' +'[' +table_col_ref.name+']' 
+			
+				from  sys.foreign_key_columns fkc_ref				
+				inner join sys.columns as table_col_ref
+				on fkc_ref.referenced_object_id=table_col_ref.object_id
+				and fkc_ref.referenced_column_id=table_col_ref.column_id				
+				where fk.object_id=fkc_ref.constraint_object_id
+				order by fkc_ref.constraint_column_id
+
+			FOR XML PATH(''),TYPE).value('(./text())[1]','VARCHAR(MAX)')
+		  ,1,2,'') COLLATE Latin1_General_CI_AS_KS_WS AS ref_table_cols
+
+
+	from sys.foreign_keys fk
+
+	inner join sys.objects as main_table
+	on fk.parent_object_id=main_table.object_id
+	
+	inner join sys.objects as ref_table
+	on fk.referenced_object_id=ref_table.object_id	
+	inner join sys.schemas as main_schema
+	on main_table.schema_id=main_schema.schema_id
+	inner join sys.schemas as ref_schema
+	on ref_table.schema_id=ref_schema.schema_id
+	
+	where @table_name = main_table.name
+	
+	) a
+	order by main_table
+	FOR XML PATH('')
+         ), 1, 0, ''),'&#x0D;',CHAR(13) ),'&gt;','>')  as code)
+
 select @query
